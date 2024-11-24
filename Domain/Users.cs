f83a8fecd37b5c5f0f7cd4b0cf28d18b4dc332a9
robelgh <robelghw123@gmail.com>
2024-11-24 08:44:56 +0300

    Add project files.

---
+++ Domain/Users.cs
@@ -0,0 +1,13 @@
+﻿using Domain.common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Users : BaseDomainEntity
+    {
+    }
+}
---
+++ Domain/common/BaseDomainEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.common
+{
+    public abstract class BaseDomainEntity
+    {
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+        public string UpdatedBy { get; set; }
+        public DateTime UpdatedDate { get; set; } = DateTime.UtcNow;
+    }
+}
